86e6662b7efe0501b021b819d062a9015de08175
MUHAMMAD TALHA <102948710+iam-talhaa@users.noreply.github.com>
2026-04-08 22:41:24 +0500

    Main screen Updated

--- lib/main.dart
+++ lib/main.dart
@@ -52,6 +52,8 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
+    print('Main Screen');
+
     return MaterialApp(home: AuthScreen());
   }
 }
